0329677973b79f5646a9beb7fc1cde576c95b9ea
陆科进 <kenlu@zhejianglab.com>
2026-06-13 10:37:29 +0800

    add start_test.sh
    Signed-off-by: 陆科进 <kenlu@zhejianglab.com>

---
+++ start_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm /workspace/output/* -fr
+
+# 把完整指令存入变量
+cmd=(
+python /workspace/segment_anything/scripts/amg.py \
+    --checkpoint /workspace/models/sam_vit_b_01ec64.pth \
+    --model-type vit_b \
+    --input /workspace/datasets/sa_8382.jpg \
+    --output /workspace/output \
+)
+
+# 先打印命令
+echo "====================================="
+echo "即将执行命令："
+echo "${cmd[@]}"
+echo "====================================="
+
+# 执行命令
+"${cmd[@]}"
